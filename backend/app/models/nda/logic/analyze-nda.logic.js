c18afb0224d7a7e9d522e582bb8a31f8ec54044b
xjunajan <mail@janjuna.cz>
2023-04-22 17:12:26 +0200

    Update scroll

--- backend/app/models/nda/logic/analyze-nda.logic.js
+++ backend/app/models/nda/logic/analyze-nda.logic.js
@@ -43,36 +43,45 @@ async function analyzeChatGPT({ input }, text) {
   const prompt2 = `Complete this json {
     "governingLaw": {
       "match": return boolean if governing law is ${input.governingLaw}
-      "line": exact line where governing law is defined,
-      "start": exact first 5 words where governing law is defined,
-      "end": exact last 5 words governing law is defined,
-      "lineNumber": document line number where governing law is defined,
     },
     "term" : {
-      "match": boolean is term set to ${input.term} years?, 
-      "line": exact line how long does the contract last,
-      "regex": return the shortest possible regular expression that searches the line,
-    },
-    "disputeMethod": {
-      "line: return line where dispute resolution method is defined,
-      "match": return boolean if dispute resolution method is ${input.disputeMethod},
-    },
-    "disclosure": {
-      "line": return line where permission of disclosure is defined,
-      "match": return boolean if dispute resolution method at least one of ${input.disclosure},
-      "match_int": return index of ${input.disclosure} that are permitted disclosure methods,
-    },
-    "exclusion": {
-      "line": return line where exclusion is defined,
-      "match": return boolean if exclusions is ${input.exclusions},
-      "match_int": return list of the following ${input.exclusions} which are true,
-    }
-    "remedies: {
-      "line": return line where remedies for breach are difend,
-      "match": return boolean if remedies for breach is ${input.remedies},
-      "match_int": which of the following remedies for breach ${input.remedies} are possible,
+      "match": boolean is term set to ${input.term} years?
     }
   }.`;
+  //
+  // const prompt2 = `Complete this json {
+  //   "governingLaw": {
+  //     "match": return boolean if governing law is ${input.governingLaw}
+  //     "line": exact line where governing law is defined,
+  //     "start": exact first 5 words where governing law is defined,
+  //     "end": exact last 5 words governing law is defined,
+  //     "lineNumber": document line number where governing law is defined,
+  //   },
+  //   "term" : {
+  //     "match": boolean is term set to ${input.term} years?,
+  //     "line": exact line how long does the contract last,
+  //     "regex": return the shortest possible regular expression that searches the line,
+  //   },
+  //   "disputeMethod": {
+  //     "line: return line where dispute resolution method is defined,
+  //     "match": return boolean if dispute resolution method is ${input.disputeMethod},
+  //   },
+  //   "disclosure": {
+  //     "line": return line where permission of disclosure is defined,
+  //     "match": return boolean if dispute resolution method at least one of ${input.disclosure},
+  //     "match_int": return index of ${input.disclosure} that are permitted disclosure methods,
+  //   },
+  //   "exclusion": {
+  //     "line": return line where exclusion is defined,
+  //     "match": return boolean if exclusions is ${input.exclusions},
+  //     "match_int": return list of the following ${input.exclusions} which are true,
+  //   }
+  //   "remedies: {
+  //     "line": return line where remedies for breach are difend,
+  //     "match": return boolean if remedies for breach is ${input.remedies},
+  //     "match_int": which of the following remedies for breach ${input.remedies} are possible,
+  //   }
+  // }.`;
 
   console.log("PROMPT:", prompt2);
   const ndaAnalyzePrompt = {
@@ -81,49 +90,49 @@ async function analyzeChatGPT({ input }, text) {
   };
 
 
-  console.log("MOCK Response");
-  const resParsed = {
-    "governingLaw": {
-      "match": false,
-      "line": "Governing law:",
-      "start": "Governing law:",
-      "end": "Governing law:",
-      "lineNumber": 17
-    },
-    "term": {
-      "match": true,
-      "line": "Confidentiality period: [Number] [years/months]",
-      "regex": "\\[Number\\] \\[years\\/months\\]"
-    },
-    "disputeMethod": {
-      "line": "Dispute Resolution Method: [Litigation in the courts of jurisdiction] [Arbitration under the rules in legal place]",
-      "match": false
-    },
-    "disclosure": {
-      "line": "The Receiver may share the Confidential Information if required by law or regulation but must promptly notify the Discloser of the requirement if allowed by law or regulation.",
-      "match": false,
-      "match_int": null
-    },
-    "exclusion": {
-      "line": "(d) Confidential Information does not include information that is: (i) in the public domain not by breach of this Agreement, (ii) known by the Receiver or its Permitted Receivers at the time of disclosure, (iii) lawfully obtained by the Receiver or its Permitted Receivers from a third party other than through a breach of confidence, (iv) independently developed by the Receiver, or (v) expressly indicated by the Discloser as not confidential.",
-      "match": false,
-      "match_int": null
-    },
-    "remedies": {
-      "line": "(g) Equitable relief. The Discloser may seek injunctive relief or specific performance to enforce its rights under this Agreement.",
-      "match": false,
-      "match_int": null
-    },
-  };
+  // console.log("MOCK Response");
+  // const resParsed = {
+  //   "governingLaw": {
+  //     "match": false,
+  //     "line": "Governing law:",
+  //     "start": "Governing law:",
+  //     "end": "Governing law:",
+  //     "lineNumber": 17
+  //   },
+  //   "term": {
+  //     "match": true,
+  //     "line": "Confidentiality period: [Number] [years/months]",
+  //     "regex": "\\[Number\\] \\[years\\/months\\]"
+  //   },
+  //   "disputeMethod": {
+  //     "line": "Dispute Resolution Method: [Litigation in the courts of jurisdiction] [Arbitration under the rules in legal place]",
+  //     "match": false
+  //   },
+  //   "disclosure": {
+  //     "line": "The Receiver may share the Confidential Information if required by law or regulation but must promptly notify the Discloser of the requirement if allowed by law or regulation.",
+  //     "match": false,
+  //     "match_int": null
+  //   },
+  //   "exclusion": {
+  //     "line": "(d) Confidential Information does not include information that is: (i) in the public domain not by breach of this Agreement, (ii) known by the Receiver or its Permitted Receivers at the time of disclosure, (iii) lawfully obtained by the Receiver or its Permitted Receivers from a third party other than through a breach of confidence, (iv) independently developed by the Receiver, or (v) expressly indicated by the Discloser as not confidential.",
+  //     "match": false,
+  //     "match_int": null
+  //   },
+  //   "remedies": {
+  //     "line": "(g) Equitable relief. The Discloser may seek injunctive relief or specific performance to enforce its rights under this Agreement.",
+  //     "match": false,
+  //     "match_int": null
+  //   },
+  // };
 
-  // const response2 = await openai.createChatCompletion({
-  //   model: GPT_MODEL_4,
-  //   messages: [
-  //     ndaAnalyzePrompt
-  //   ],
-  //   temperature: 0,
-  // });
-  // const resParsed = response2.data.choices[0].message.content;
+  const response2 = await openai.createChatCompletion({
+    model: GPT_MODEL_4,
+    messages: [
+      ndaAnalyzePrompt
+    ],
+    temperature: 0,
+  });
+  const resParsed = JSON.parse(response2.data.choices[0].message.content);
 
   console.log("RESPONSE:", resParsed);
   return {
